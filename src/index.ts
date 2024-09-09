596ac56be1c6ee25277eccbf8c975fa815d61cc0
Sungjun Hong <hongsi140626@gmail.com>
2024-09-09 18:16:11 +0900

    msa

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,4 @@
 */
 import("./bootstrap");
 
-// To solve ts error => '--isolatedModules' because it is considered a global script file
-// Or => Alternatively, set the 'isolatedModules' property to 'true' into 'tsconfig.json' file.
 export {};
